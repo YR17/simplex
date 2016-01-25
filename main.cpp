08d0e4fad6b67fd708b3da00da6705727646db71
Yurii Rivnyi <yuriyrivniy@gmail.com>
2016-01-25 15:40:15 +0200

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+																	//x1, x2, x3, S1, S2, S3, Res 
+	vector<vector<float> > matrix = {
+												/* F*/		 {10, -7, -5, 0, 0, 0, 0},	
+																	 {6, 15, 6,   1, 0, 0, 9},
+																	 {14, 42, 16, 0, 1, 0, 21},
+																	 {2, 8, 2,		0, 0, 1, 4}
+																	};
+	return 0;
+}
